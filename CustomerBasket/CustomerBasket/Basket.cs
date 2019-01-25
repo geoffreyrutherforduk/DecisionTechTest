7534face8a5047134b217a47b2251b57b5d2ee8c
geoffreyrutherforduk <geoffrey.rutherford.uk@gmail.com>
2019-01-25 07:17:10 +0000

    Initial commit. Added new basket with basket tests

---
+++ CustomerBasket/CustomerBasket/Basket.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using static CustomerBasket.Product;
+
+namespace CustomerBasket
+{
+    public class Basket
+    {
+        private Dictionary<ProductType, int> _products = new Dictionary<ProductType, int>();
+        
+        public void AddToBasket(ProductType product, int quantity = 1)
+        {
+            if (quantity < 1)
+            {
+                return;
+            }
+
+            if (_products.TryGetValue(product, out int currentQuantity))
+            {
+                currentQuantity += quantity;
+                _products[product] = currentQuantity;
+            }
+            else
+            {
+                _products.Add(product, quantity);
+            }
+        }
+
+        public int GetProductQuantity(Product.ProductType product)
+        {
+            _products.TryGetValue(product, out int productQty);
+            return productQty;
+        }
+    }
+}
